4bee04861f8dd6d4aa37cb50701ed1ddcee05a53
José Pedro de Santana Neto <1jpsneto@gmail.com>
2021-12-11 05:02:24 +0100

    Code can run but I need to check the unit system.

--- examples/benchmark_rheological_yann/benchmark.cpp
+++ examples/benchmark_rheological_yann/benchmark.cpp
@@ -165,7 +165,8 @@ int main(int argc, char* argv[]) {
 
     defineDynamics(lattice,lattice.getBoundingBox(),new DYNAMICS);    
     
-    const T maxT = 30.0/strain_rate; // Yann's thesis = 20.0/strain_rate
+    //const T maxT = 30.0/strain_rate; // Yann's thesis = 20.0/strain_rate
+    const T maxT = 0.0; // Yann's thesis = 20.0/strain_rate
     //const T vtkT = 0.1;
     //const T vtkT = 1.4;
     const T vtkT = 4.0;

5588e248c594bc7311e491be89a1d0a31460102d
José Pedro de Santana Neto <1jpsneto@gmail.com>
2022-02-05 03:04:37 +0100

    Creating relative apparent viscosity measurements setup.

--- examples/benchmark_rheological_Krieger_Dougherty/benchmark.cpp
+++ examples/benchmark_rheological_Krieger_Dougherty/benchmark.cpp
@@ -18,30 +18,6 @@
  * Author: Philippe Seil (philippe.seil@jku.at)
  */
 
-/*
-This case was used as a benchmark. It consists of a tank with fluid 
-where the upper third is filled with particles that then settle under
-gravity. The command line parameter v_inf is an estimated settling velocity.
-If chosen too large, the timestep will be very small. If chosen too small,
-the simulation will crash.
-
-A few working parameter sets:
-
-./benchmark 0.1 5 0.1 1e-4 0.15 0.02 your/out/dir
-results in ~70 particles on a 51x51x101 grid. Feasible on a desktop computer.
-Particle Reynolds number ~150
-
-./benchmark 0.06 5 0.1 1e-4 0.10 0.02 your/out/dir
-~340 particles on a 84x84x164 grid - might need 1-2h or so on a desktop computer.
-Particle Reynolds number ~90
-
-
-./benchmark 0.02 5 0.1 1e-5 0.15 0.02 your/out/dir 
-gives you ~10k particles on a 251x251x501 grid - computationally demanding!
-Particle Reynolds number ~300
-
- */
-
 #include "palabos3D.h"
 #include "palabos3D.hh"
 
@@ -106,7 +82,7 @@ int main(int argc, char* argv[]) {
     plbInit(&argc, &argv);
     
     const T d_part = 0.1; // 1 - particle diameter
-    const plint N = 5; // 2 - number of grid points per particle diameter
+    const plint N = 10; // 2 - number of grid points per particle diameter
     const T v_frac = 0.5; // 3 - the solid fraction in the insertion region
     const T nu_f = 1e-4; // 4 - kinematic viscosity (m^2/s)
     const T v_inf = 0.15; // 5 - velocity of the particle (v_inf is an estimated settling velocity)
@@ -116,11 +92,18 @@ int main(int argc, char* argv[]) {
     const std::string outDir = "outDir/"; /* 7 - directory where your output shall be stored. It must
     contain the subdirectories outDir/post and outDir/tmp. */
 
+    const T lx = 2., ly = 2., lz = 2.;
+    const T strain_rate = 0.00005;
+    const T velocity_imposed = strain_rate*ly;
+    const T rho_f = 1000;
+    const T r_ = d_part/2.;
+    const T rho_s = 1100.;
+    const T m = r_*r_*r_*4./3.*3.14*rho_s;
+    const T Rep = d_part*velocity_imposed/nu_f;
+
     std::string lbOutDir(outDir), demOutDir(outDir);
     lbOutDir.append("tmp/"); demOutDir.append("post/");
     global::directories().setOutputDir(lbOutDir);
-
-    const T rho_f = 1000;
 
     LiggghtsCouplingWrapper wrapper(argv,global::mpi().getGlobalCommunicator());
 
@@ -128,23 +111,11 @@ int main(int argc, char* argv[]) {
     // as variables (see LIGGGHTS docu for details)
     wrapper.setVariable("r_part",d_part/2);
     wrapper.setVariable("v_frac",v_frac);
-    
     wrapper.execFile("in.lbdem");
-
-
-    T g = 9.81;
-
-    const T lx = 2., ly = 2., lz = 2.;
-
-
-    T r_ = d_part/2.;
-    T rho_s = 1100.;
-    T m = r_*r_*r_*4./3.*3.14*rho_s;
     
     PhysUnits3D<T> units(2.*r_,v_inf,nu_f,lx,ly,lz,N,uMax,rho_f);
 
     IncomprFlowParam<T> parameters(units.getLbParam());
-
     
     ///// Initialize the diagonal relaxation matrix from the .xml file.
     // Q19 and Q27 do not have the same number of relaxation parameters!
@@ -155,7 +126,6 @@ int main(int argc, char* argv[]) {
     allOmega[3] = parameters.getOmega();    // relaxation of M220 and cyclic permutations
     allOmega[4] = 1.0; // relaxation of bulk moment (M200 + M020 + M002)
     RRdynamics<T,DESCRIPTOR>::allOmega = allOmega;
-    
 
     plint nx = parameters.getNx(), ny = parameters.getNy(), nz = parameters.getNz();
 
@@ -176,7 +146,6 @@ int main(int argc, char* argv[]) {
 
     defineDynamics(lattice,lattice.getBoundingBox(),new DYNAMICS);
     
-    
     const T maxT = ceil(3.*lz/v_inf);
     // const T vtkT = 0.1;
     // const T vtkT = 0.000000001;
@@ -196,7 +165,7 @@ int main(int argc, char* argv[]) {
     Box3D inlet(0,0,0,ny-1,0,nz-1), outlet(nx-1,nx-1,0,ny-1,0,nz-1);
 
     // set strain rate
-    T vel = units.getLbVel(0.1);
+    T vel = units.getLbVel(velocity_imposed);
     //T vel = units.getLbVel(v_inf);
     Box3D lid(0, nx - 1, ny - 1, ny - 1, 0, nz - 1);
     Box3D bottom(0, nx - 1, 0, 0, 0, nz - 1);
@@ -207,10 +176,6 @@ int main(int argc, char* argv[]) {
     boundaryCondition->addVelocityBoundary1N(lid, lattice);
     setBoundaryVelocity(lattice, lid, Array<T, 3>(vel, 0., 0.));
     setBoundaryVelocity(lattice, bottom, Array<T, 3>(0., 0., 0.));
-    
-    T deltaRho = 0.005;
-    // T rhoHi = 1.+0.5*deltaRho, rhoLo = 1.-0.5*deltaRho;
-    T rhoHi = 1., rhoLo = 1.-deltaRho;
 
     // initializeAtEquilibrium( lattice, lattice.getBoundingBox(), 
     //                          PressureGradient<T>(rhoHi,rhoLo,nz,0) );
@@ -227,11 +192,14 @@ int main(int argc, char* argv[]) {
     pcout << "------------------------------\n"
           << "omega: " << parameters.getOmega() << "\n"
           << "dt_phys: " << dt_phys << "\n"
+          << "dx_phys: " << units.getPhysLength(1) << "\n"
           << "maxT: " << maxT << " | maxSteps: " << maxSteps << "\n"
           << "v_inf: " << v_inf << "\n"
-          << "Re : " << parameters.getRe() << "\n"
+          << "Rep : " << Rep << "\n"
           << "vtkT: " << vtkT << " | vtkSteps: " << vtkSteps << "\n"
           << "grid size: " << nx << " " << ny << " " << nz << "\n"
+          << "strain_rate: " << strain_rate << "\n"
+          << "velocity_imposed: " << velocity_imposed << "\n"
           << "------------------------------" << std::endl;
     // set timestep and output directory
     wrapper.setVariable("t_step",dt_dem);
@@ -272,6 +240,32 @@ int main(int argc, char* argv[]) {
     ofile_velocity_gradient_x << std::endl;
     // --------------------------------------
 
+    // Relative Apparent Viscosity
+    std::string fname_relative_apparent_viscosity(
+      global::directories().getOutputDir() + "relative_apparent_viscosity.csv");
+    plb_ofstream ofile_relative_apparent_viscosity(fname_relative_apparent_viscosity.c_str());
+    ofile_relative_apparent_viscosity << "iT";
+    for (plint iY = 0; iY < ny - 1; ++iY) {
+      ofile_relative_apparent_viscosity << "," << iY;
+    }
+    ofile_relative_apparent_viscosity << std::endl;
+    // --------------------------------------
+
+    // Relative Apparent Viscosity Averaged
+    T measurements_relative_apparent_viscosity_averaged = 0;
+    T averaged_velocities[ny]; averaged_velocities[ny - 1] = 0;
+    std::string fname_relative_apparent_viscosity_averaged(
+      global::directories().getOutputDir() + "relative_apparent_viscosity_averaged.csv");
+    plb_ofstream ofile_relative_apparent_viscosity_averaged(
+      fname_relative_apparent_viscosity_averaged.c_str());
+    ofile_relative_apparent_viscosity_averaged << "iT";
+    for (plint iY = 0; iY < ny - 1; ++iY) {
+      ofile_relative_apparent_viscosity_averaged << "," << iY;
+      averaged_velocities[iY] = 0;
+    }
+    ofile_relative_apparent_viscosity_averaged << std::endl;
+    // --------------------------------------
+
     // Loop over main time iteration.
     for (plint iT=0; iT<=maxSteps; ++iT) {
 
@@ -302,26 +296,48 @@ int main(int argc, char* argv[]) {
                                                     0))) - 
                                   units.getPhysVel(computeAverage(*computeVelocityComponent(lattice,
                                                     Box3D(0, nx - 1, iY, iY, 0, nz - 1),
-                                                    0))) )/units.getPhysLength(1);;
+                                                    0))) )/units.getPhysLength(1);
           ofile_velocity_gradient_x << "," << setprecision(10) << velocity_gradient;
         }
         ofile_velocity_gradient_x << std::endl;
+
+        // Relative Apparent Viscosity
+        ofile_relative_apparent_viscosity << iT;
+        for (plint iY = 0; iY < ny - 1; ++iY) {
+          T velocity_gradient = ( units.getPhysVel(computeAverage(*computeVelocityComponent(lattice,
+                                                    Box3D(0, nx - 1, iY + 1, iY + 1, 0, nz - 1),
+                                                    0))) - 
+                                  units.getPhysVel(computeAverage(*computeVelocityComponent(lattice,
+                                                    Box3D(0, nx - 1, iY, iY, 0, nz - 1),
+                                                    0))) )/units.getPhysLength(1);
+          T relative_apparent_viscosity = velocity_gradient/strain_rate;
+          ofile_relative_apparent_viscosity << "," << setprecision(10) << relative_apparent_viscosity;
+        }
+        ofile_relative_apparent_viscosity << std::endl;
+
+        // Relative Apparent Viscosity Averaged
+        for (plint iY = 0; iY < ny; ++iY) {
+          averaged_velocities[iY] += units.getPhysVel(computeAverage(*computeVelocityComponent(lattice,
+                                                    Box3D(0, nx - 1, iY, iY, 0, nz - 1),
+                                                    0)));          
+        }
+        measurements_relative_apparent_viscosity_averaged++;
+        ofile_relative_apparent_viscosity_averaged << iT;
+        for (plint iY = 0; iY < ny - 1; ++iY) {
+          T velocity_gradient = averaged_velocities[iY + 1] - averaged_velocities[0];
+          velocity_gradient /= measurements_relative_apparent_viscosity_averaged; // averaging in fact
+          T distance = (iY + 1)*units.getPhysLength(1); //delta position in physical units ((iY + 1) is the number of cells (LB units))
+          T meanStrainRate = velocity_gradient / distance; //measured strain rate in physical units
+          T relative_apparent_viscosity = meanStrainRate/strain_rate;
+          ofile_relative_apparent_viscosity_averaged << "," << setprecision(10) << relative_apparent_viscosity;
+        }
+        ofile_relative_apparent_viscosity_averaged << std::endl;
       }
 
       T rhoAvgIn = computeAverageDensity(lattice,inlet);
       T rhoAvgOut = computeAverageDensity(lattice,outlet);
 
       lattice.collideAndStream();
-
-      /*
-      // applying a pressure gradient across the periodic boundary 
-      applyProcessingFunctional
-      (new ZhangPeriodicPressureFunctional3D<T,DESCRIPTOR>(rhoHi, 
-                       rhoAvgOut,0,1), inlet, lattice);
-      applyProcessingFunctional
-      (new ZhangPeriodicPressureFunctional3D<T,DESCRIPTOR>(rhoLo, 
-                       rhoAvgIn,0,-1), outlet, lattice);
-      */
 
       getForcesFromLattice(lattice,wrapper,units);
 
@@ -348,4 +364,6 @@ int main(int argc, char* argv[]) {
     ofile_energy.close();
     ofile_velocity_x.close();
     ofile_velocity_gradient_x.close();
+    ofile_relative_apparent_viscosity.close();
+    ofile_relative_apparent_viscosity_averaged.close();
 }
